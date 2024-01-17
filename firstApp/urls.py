fab11e0dd3ec1a33392916ff4408863ed3dc450f
rafees <rafees99@gmail.com>
2024-01-17 19:55:30 +0530

    CRUD operation using modelform

--- firstApp/urls.py
+++ firstApp/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "firstApp"
 urlpatterns = [
     path('create',views.create,name="create"),
-    path('edit',views.edit,name="edit"),
-    path('list',views.list,name="list"),
+    path('edit/<pk>',views.edit,name="edit"),
+    path('',views.list,name="list"),
+    path('delete/<pk>',views.delete,name="delete"),
 ]
--- firstApp/views.py
+++ firstApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MovieInfo
 from django.http import HttpResponse
 from .forms import MovieInfoForm
@@ -26,5 +26,27 @@ def list(request):
     return render(request, "firstApp/list.html", {"movies":movie_details})
 
 
-def edit(request):
-    return render(request, "firstApp/edit.html")
+def edit(request,pk):
+    edit_obj = MovieInfo.objects.get(id=pk)
+    if request.method == "POST":
+        frm = MovieInfoForm(request.POST,instance=edit_obj)
+        if frm.is_valid():
+            frm.save()
+        # title = request.POST.get("title")
+        # year = request.POST.get("year")
+        # summary = request.POST.get("summary")
+        # edit_obj.title = title
+        # edit_obj.year = year
+        # edit_obj.summary = summary
+        # edit_obj.save()
+        return redirect("firstApp:list")
+    else:
+        form = MovieInfoForm(instance=edit_obj)
+    # return render(request, "firstApp/edit.html",{"edit_item":edit_obj})
+    return render(request, "firstApp/create.html", {"form": form})
+def delete(request,pk):
+    del_obj = MovieInfo.objects.get(id=pk)
+    del_obj.delete()
+    movie_details = MovieInfo.objects.all()
+    print(movie_details)
+    return render(request, "firstApp/list.html", {"movies": movie_details})
