0a1a7fc049f7639ca6a50ec20231a428994e7b24
rafees <rafees99@gmail.com>
2024-01-18 13:59:09 +0530

    oneToOne,manyToone,manyTomany relations added

--- firstApp/admin.py
+++ firstApp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import  MovieInfo
-# Register your models here.
+from .models import  MovieInfo,Director,CensorInfo,Actor
 class MoviesInfoAdmin(admin.ModelAdmin):
     list_display = ["title","year"]
 admin.site.register(MovieInfo,MoviesInfoAdmin)
+admin.site.register(Director)
+admin.site.register(CensorInfo)
+admin.site.register(Actor)
 
--- firstApp/models.py
+++ firstApp/models.py
@@ -2,10 +2,25 @@ from django.db import models
 
 
 # Create your models here.
+class Director(models.Model):
+    director = models.CharField(max_length=200)
+    def __str__(self):
+        return self.director
+class Actor(models.Model):
+    actor=models.CharField(max_length=50)
+    def __str__(self):
+        return self.actor
+class CensorInfo(models.Model):
+    rating = models.CharField(max_length=10)
+    certified_by = models.CharField(max_length=200,null=True)
+
 class MovieInfo(models.Model):
     title = models.CharField(max_length=100)
     year = models.IntegerField(null=True)
     summary = models.TextField()
+    censor_details = models.OneToOneField(CensorInfo,on_delete=models.SET_NULL,related_name='movie',null=True)
+    directed_by  = models.ForeignKey(Director,null=True,on_delete=models.CASCADE,related_name='directed_movies')
+    acted_by = models.ManyToManyField(Actor,related_name="acted_movies")
     image = models.ImageField(upload_to='uploads/', null=True,blank=True)
 
     def __str__(self):
